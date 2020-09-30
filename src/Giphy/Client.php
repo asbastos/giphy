97f423828e00e2f23e51246a3053b6f52b23ab78
Artur Bastos <asbastos90@gmail.com>
2020-09-29 21:10:11 -0400

    fourth update

--- src/Giphy/Client.php
+++ src/Giphy/Client.php
@@ -2,10 +2,17 @@
 
 namespace Giphy;
 
+use GuzzleHttp\Client as GuzzleClient;
+
 class Client {
+    private $client;
+    private const BASE_URI = 'api.giphy.com/v1';
 
     public function __construct()
     {
-
+        $this->client = new GuzzleClient([
+            'base_uri' => self::BASE_URI,
+            'timeout' => 2.0,
+        ]);
     }
 }
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -50,6 +50,24 @@ class ComposerAutoloaderInitb165797c52e80abb0bf493ad9e3c6383
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInitb165797c52e80abb0bf493ad9e3c6383::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequireb165797c52e80abb0bf493ad9e3c6383($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequireb165797c52e80abb0bf493ad9e3c6383($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
